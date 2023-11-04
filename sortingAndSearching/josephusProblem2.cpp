a80996e2e66ea13f850b8d8fff3800e78171ddfc
Ashish0253 <ashishrana0253@gmail.com>
2023-11-04 23:57:26 +0530

    josephusProbelm done but use used policy based ds

---
+++ sortingAndSearching/josephusProblem2.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define gc getchar_unlocked
+#define fo(i, a, n) for (ll i = a; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> st;
+
+void solve() {
+    
+    int n, k;
+    cin >> n >> k;
+    
+    fo(i, 0, n) st.insert(i + 1);
+
+    int idx = k;
+    while(st.size()) {
+
+        idx %= st.size();
+
+        int x = *st.find_by_order(idx);
+        st.erase(x);
+
+        cout << x << " ";
+        
+        idx += k;
+    }  
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+        solve();
+    // }
+    return 0;
+}
